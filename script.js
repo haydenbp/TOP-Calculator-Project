67125f1517202842a5458d9ac675d7476a8b47a1
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-02-17 20:37:18 -0500

    basic functions...functioning

--- script.js
+++ script.js
@@ -51,3 +51,59 @@ function operate(operator,num1,num2){
 
 
 }
+
+const clearButton = document.getElementById('clear');
+
+const numberButtons = document.querySelectorAll('.number');
+
+const equalButton = document.getElementById('equals');
+
+const display = document.getElementById('display');
+
+const operatorButtons = document.querySelectorAll('.operator');
+
+let operator = null;
+
+let number = null;
+
+clearButton.addEventListener('click', ()=>{
+
+    display.innerText = '';
+
+})
+
+
+
+numberButtons.forEach(item => item.addEventListener('click',()=>{
+
+    display.innerText += item.innerText;
+    //number = parseInt(display.innerText)
+    console.log(number);
+}
+
+))
+
+operatorButtons.forEach(item => item.addEventListener('click',()=>{
+
+    operator = item.innerText
+    number = parseInt(display.innerText)
+    display.innerText = ''
+    console.log(operator);
+}
+
+))
+
+equalButton.addEventListener('click', ()=>{
+
+    let numberTwo = parseInt(display.innerText)
+
+    console.log(operator,number,numberTwo);
+
+    display.innerText = `${number} ${operator} ${numberTwo} = ${operate(operator,number,numberTwo)}`
+    console.log(display.innerText);
+
+})
+
+
+
+console.log(operatorButtons)
